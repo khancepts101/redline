a8ea2fc3595339b439386fa016ddd04ea1b675a6
khancepts101 <khancepts101@users.noreply.github.com>
2026-07-14 00:26:54 -0400

    Expose private Pushgateway to CardHunt jobs

--- infra/main.tf
+++ infra/main.tf
@@ -55,6 +55,15 @@ resource "aws_vpc_security_group_ingress_rule" "cardhunt_glitchtip" {
   to_port                      = 8000
 }
 
+resource "aws_vpc_security_group_ingress_rule" "cardhunt_pushgateway" {
+  security_group_id            = aws_security_group.redline.id
+  referenced_security_group_id = var.cardhunt_security_group_id
+  description                  = "Pushgateway ingestion from CardHunt ECS batch tasks"
+  ip_protocol                  = "tcp"
+  from_port                    = 9091
+  to_port                      = 9091
+}
+
 resource "aws_instance" "redline" {
   ami                         = data.aws_ami.ubuntu_arm64.id
   instance_type               = var.instance_type
